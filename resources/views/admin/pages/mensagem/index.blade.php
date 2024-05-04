ddf5e5ff0e17825904c69588c028b0c6861f5f15
Murillo Lima <murillo_smit@hotmail.com>
2024-05-04 12:54:15 -0300

    mensagens

--- resources/views/admin/pages/mensagem/index.blade.php
+++ resources/views/admin/pages/mensagem/index.blade.php
@@ -83,70 +83,7 @@
                         </div>
                     </div>
                 @else
-                    <div class="row">
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-info">
-                                <div class="inner">
-                                    <h3>{{ count($noticias) }}</h3>
-                                    <p>NOTÍCIAS</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
-                                </div>
-                                <a href="{{ route('admn.pages.noticias.index') }}" class="small-box-footer">+info <i
-                                        class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-success">
-                                <div class="inner">
-                                    <h3>53</h3>
-                                    <p>Congresssos</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">+info <i
-                                        class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-warning">
-                                <div class="inner">
-                                    <h3>{{ count($users) }}</h3>
-                                    <p>SERVIDORES</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-person-add"></i>
-                                </div>
-                                <a href="{{ route('admin.pages.users.index') }}" class="small-box-footer">+info <i
-                                        class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-danger">
-                                <div class="inner">
-                                    <h3>{{ count($reunioes) }}</h3>
-
-                                    <p>REUNIÕES</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
-                                </div>
-                                <a href="{{ route('admin.pages.reunioes.index') }}" class="small-box-footer">+info <i
-                                        class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                    </div>
-                    <!-- /.row -->
+                    
                 @endif
 
 
