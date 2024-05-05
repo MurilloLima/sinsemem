7c42dc161c3469daf3b85dcd9d829e1d446e42f1
Murillo Lima <murillo_smit@hotmail.com>
2024-05-04 21:46:36 -0300

    foto perfil no admin nav left

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -12,7 +12,8 @@
          <!-- Sidebar user panel (optional) -->
          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
              <div class="image">
-                 <img src="{{ asset('upload/fotoperfil/perfil.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                 <img src="{{ asset('upload/fotoperfil/' . auth()->user()->img) }}" class="img-circle elevation-2"
+                     alt="User Image">
              </div>
              <div class="info">
                  <a href="#" class="d-block">{{ auth()->user()->name }}</a>
